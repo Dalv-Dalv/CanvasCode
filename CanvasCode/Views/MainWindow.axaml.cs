839921fbc156723a82838ff4a44f6180130d1fc2
Dalv <futurevlad2@gmail.com>
2025-07-02 16:20:16 +0300

    Testing

--- CanvasCode/Views/MainWindow.axaml.cs
+++ CanvasCode/Views/MainWindow.axaml.cs
@@ -1,11 +1,15 @@
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace CanvasCode.Views;
 
-public partial class MainWindow : Window
-{
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+public partial class MainWindow : Window {
+	public MainWindow() {
+		InitializeComponent();
+	}
+
+	private void Button_OnClick(object? sender, RoutedEventArgs e) {
+		Debug.WriteLine("Click!");
+	}
 }
